3030438b15d63bbbf806c3201530401ce0dd0cad
Aruzhan <98633567+arumeiirkhan@users.noreply.github.com>
2023-01-27 15:16:22 +0600

    abc

---
+++ Lab1/khanacademy/Intro to HTML/1.go
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Challenge: Write a Poem</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+    <h1>Doride</h1>
+    <p>I believe: I love; for the heart is to be believed.<br>
+Not, my dear, can not be a hypocrite;<br>
+All genuinely in it: desires languid heat,<br>
+shyness timid, Harith priceless gift,<br>
+Dresses and nice speeches negligence<br>
+And affectionate names infant tenderness.
+</p>
+    </body>
+</html>
