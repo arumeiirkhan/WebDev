ddf45d9b9984a2f38373a06a3402d15974c1fc63
Aruzhan <98633567+arumeiirkhan@users.noreply.github.com>
2023-01-27 15:17:32 +0600

    Write a Poem

--- Lab1/khanacademy/Intro to HTML/1.go
+++ Lab1/khanacademy/Intro to HTML/1.go
@@ -7,11 +7,11 @@
     <body>
     <h1>Doride</h1>
     <p>I believe: I love; for the heart is to be believed.<br>
-Not, my dear, can not be a hypocrite;<br>
-All genuinely in it: desires languid heat,<br>
-shyness timid, Harith priceless gift,<br>
-Dresses and nice speeches negligence<br>
-And affectionate names infant tenderness.
-</p>
+       Not, my dear, can not be a hypocrite;<br>
+       All genuinely in it: desires languid heat,<br>
+       shyness timid, Harith priceless gift,<br>
+       Dresses and nice speeches negligence<br>
+       And affectionate names infant tenderness.
+    </p>
     </body>
 </html>
